40de921737247073df96fd17d69d3381f4f2bb5e
Ngo Viet Hung <hung.ngo@asiantech.vn>
2017-04-18 15:05:47 +0700

    Edit bids migration

--- database/migrations/2017_04_17_045628_create_bids_table.php
+++ database/migrations/2017_04_17_045628_create_bids_table.php
@@ -19,8 +19,8 @@ class CreateBidsTable extends Migration
             $table->timestamps();
         });
         Schema::table('bids', function (Blueprint $table) {
-            $table->integer('user_id')->unsigned();
-            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->integer('order_id')->unsigned();
+            $table->foreign('order_id')->references('id')->on('orders')->onDelete('cascade');
             $table->integer('shipper_id')->unsigned();
             $table->foreign('shipper_id')->references('id')->on('users')->onDelete('cascade');
         });
